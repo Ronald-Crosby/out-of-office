717e4ae262a919920a230a59e2ed4e0c372fc3d7
Olly Robinson <ollythedeveloper@gmail.com>
2020-04-12 15:07:49 +0100

    half way

---
+++ sketch.js
@@ -0,0 +1,44 @@
+let font;
+let graphic;
+
+var canvasDiv = document.getElementById("canvas-container");
+let width = canvasDiv.offsetWidth;
+
+function preload() {
+  font = loadFont("spacegrotesk-medium.otf");
+}
+
+function setup() {
+  var sketchCanvas = createCanvas(width, 600);
+  sketchCanvas.parent("canvas-container");
+
+  graphic = createGraphics(1200, 600);
+  graphic.textFont(font);
+  graphic.textSize(500);
+  graphic.textAlign(CENTER, CENTER);
+  graphic.fill("#ffffff");
+  graphic.text("ooo", 600, 260);
+}
+
+function draw() {
+  background("#000000");
+  tileSize = 50;
+
+  for (let i = 0; i < 12; i++) {
+    // this will be between 0 and 1 depending on the x position of the mouse across the screen
+    const mousePosition = 0.9;
+
+    const sx = 0;
+    const sy = i * tileSize * mousePosition;
+    const sw = 1200;
+    const sh =
+      tileSize * mousePosition + (600 - tileSize) * (1 - mousePosition);
+
+    const dx = 0;
+    const dy = i * tileSize;
+    const dw = 1200;
+    const dh = tileSize;
+
+    image(graphic, dx, dy, dw, dh, sx, sy, sw, sh);
+  }
+}
